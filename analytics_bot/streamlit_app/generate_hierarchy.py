aa388061ee55695fd82c9830f19a2533dc0f57cc
thickett <82325813+thickett@users.noreply.github.com>
2024-01-16 18:22:58 +0000

    Create generate_hierarchy.py

---
+++ analytics_bot/streamlit_app/generate_hierarchy.py
@@ -0,0 +1,9 @@
+## script to generate hierarchy of databases supplied by a given config file
+from helper_functions.DBconnector import DBConnector
+from helper_functions.get_credentials import get_credentials
+
+db_connector = DBConnector()
+hierarchy = db_connector.get_schema_hierarchy()
+
+with open("./hierarchy.txt", "w") as f:
+    f.write(hierarchy)
