9aec4774990dad3589f606842aa2e7308ed27981
thickett <82325813+thickett@users.noreply.github.com>
2024-01-17 17:58:51 +0000

    Delete analytics_bot/streamlit_app/generate_hierarchy.py

--- analytics_bot/streamlit_app/generate_hierarchy.py
+++
@@ -1,9 +0,0 @@
-## script to generate hierarchy of databases supplied by a given config file
-from helper_functions.DBconnector import DBConnector
-from helper_functions.get_credentials import get_credentials
-
-db_connector = DBConnector()
-hierarchy = db_connector.get_schema_hierarchy()
-
-with open("./hierarchy.txt", "w") as f:
-    f.write(hierarchy)
